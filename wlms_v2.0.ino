08630d13e2034d012a47cd7513c88df893df4c71
ME-TECH-ELECTRONICS <melvin.rt.123@gmail.com>
2024-05-28 22:34:44 +0530

    v1.0

--- wlms_v2.0.ino
+++ wlms_v2.0.ino
@@ -1,1 +1,22 @@
 /* Include Libaray*/
+#include <WiFi.h>
+#include <esp_now.h>
+#include <SPI.h>
+#include <SD.h>
+#include <Wire.h>
+#include <RTClib.h>
+#include <LiquidCrystal_I2C.h>
+
+// Declare Global Variables and GPIO pins
+const int AC_VOLTAGE_SENSOR_PIN 34
+const int SD_CS_PIN 5
+const int MOTOR_RELAY_PIN 32
+const int AC_SUPPLY_LED_PIN 25
+const int AUTO_MODE_LED_PIN 26
+const int MOTOR_ON_LED_PIN 27
+const int LOW_VOLTAGE_LED_PIN 14
+const int BUZZER_PIN 13
+
+
+File myFile;
+
