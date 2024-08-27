fc5583c9f279f143926860bf2bea4a7ee8181682
ME-TECH-ELECTRONICS <melvin.rt.123@gmail.com>
2024-08-27 22:40:57 +0530

    v1.0

--- WSN/WSN.ino
+++ WSN/WSN.ino
@@ -1,14 +1,29 @@
+#include <SPI.h>
+#include <RF24.h>
+
 
 const int trigPin = 0;
 const int echoPin = 1;
+const byte address[6] = "00001";
+
+struct DataPacket {
+  int integerValue;
+  float floatValue;
+};
+
+RF24 radio(9, 10); // CE, CSN
 
 void setup() {
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
+    radio.begin();
+    radio.openWritingPipe(address);
+    radio.setPALevel(RF24_PA_MIN);
+    radio.stopListening();
 }
 
 void loop() {
-
+    radio.write(&data, sizeof(data));
 }
 
 int getDistance() {
