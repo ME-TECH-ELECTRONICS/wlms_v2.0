ccd4f113153685f4f9f781820a6cebf83180d8ef
ME TECH <melvin.rt.123@gmail.com>
2024-09-15 21:59:45 +0530

    V1.0

--- WSN/WSN.ino
+++ WSN/WSN.ino
@@ -1,4 +1,71 @@
 #include <SPI.h>
+#include <LoRa.h>
+
+//define the pins used by the transceiver module
+#define ss 10
+#define rst 9
+#define dio0 2
+
+int counter = 0;
+struct RxData {
+    int distance;
+};
+ RxData data;
+int distance;
+void setup() {
+  //initialize Serial Monitor
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("LoRa Sender");
+
+  //setup LoRa transceiver module
+  LoRa.setPins(ss, rst, dio0);
+  
+  //replace the LoRa.begin(---E-) argument with your location's frequency 
+  //433E6 for Asia
+  //868E6 for Europe
+  //915E6 for North America
+  while (!LoRa.begin(433E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+   // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    // RxData data;
+    
+    // Read the user input as an integer
+    distance = Serial.parseInt();
+    
+    // Begin packet
+    
+
+    // Serial.print("Transmitted distance: ");
+    // Serial.println(data.distance);
+
+    // Serial.println("Enter the distance value:");
+  }
+data.distance = distance;
+  LoRa.beginPacket();
+  LoRa.write(distance);
+    // Transmit data as bytes
+    // LoRa.write((uint8_t*)&data, sizeof(data));
+
+    // End packet
+    LoRa.endPacket();
+
+    delay(1000);
+}
+
+
+
+/*#include <SPI.h>
 #include <RF24.h>
 
 const int trigPin = 0;
@@ -46,4 +113,4 @@ int getDistance() {
 
 float getTemperature() {
     
-}
+}*/
---
+++ tt/tt.ino
@@ -0,0 +1,48 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+//define the pins used by the transceiver module
+#define ss 5
+#define rst 15
+#define dio0 2
+struct RxData {
+    int distance;
+};
+void setup() {
+  //initialize Serial Monitor
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("LoRa Receiver");
+
+  //setup LoRa transceiver module
+  LoRa.setPins(ss, rst, dio0);
+  
+  //replace the LoRa.begin(---E-) argument with your location's frequency 
+  //433E6 for Asia
+  //868E6 for Europe
+  //915E6 for North America
+  while (!LoRa.begin(433E6)) {
+    Serial.println(".");
+    delay(500);
+  }
+   // Change sync word (0xF3) to match the receiver
+  // The sync word assures you don't get LoRa messages from other LoRa transceivers
+  // ranges from 0-0xFF
+  LoRa.setSyncWord(0xF3);
+  Serial.println("LoRa Initializing OK!");
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    RxData receivedData;
+
+    // Read the incoming bytes into the struct
+    LoRa.readBytes((uint8_t*)&receivedData, sizeof(receivedData));
+
+    // Print the received distance value
+    Serial.print("Received distance: ");
+    Serial.println(receivedData.distance);
+  }
+}
